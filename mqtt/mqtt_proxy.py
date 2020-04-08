cde3498609ce2df4d64cb35ba68001619ffc1f7c
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2020-04-08 18:55:34 -0400

    fix: mqtt proxy now in a functioning state. Still neeeds: - deduplication of retained messages on boot - handlers for transmitting to ElasticSearch - rebuild EleasticSearch index script? - reconnect handler from the sensor after sensor clock ticks roll over since the clock isn't static.

--- mqtt/mqtt_proxy.py
+++ mqtt/mqtt_proxy.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import json
 import signal
 import sys
+import traceback
 
 DB = sqlite3.connect('soundsensordata.db')
 DB.execute('create table if not exists readings (sensor unsigned int not null, iot_time unsigned int, t timestamp default CURRENT_TIMESTAMP, adc int not null default 0)')
@@ -20,18 +21,24 @@ def disconnect_sensor(sensorid, data):
 
 def sensor_reading(sensorid, data):
     """construct the reading SQL insert and execute it"""
-    DB.execute('insert into readings values (?, ?, CURRENT_TIMESTAMP, ?)', (int(Sensorid), data['adc'],))
+    DB.execute('insert into readings values (?, ?, CURRENT_TIMESTAMP, ?)', (int(sensorid), int(data['t']), data['adc'],))
     DB.commit()
 
 def sensor_state(sensorid, data):
-    """route sensor state date to connect/disconnect"""
-    pass
+    """route sensor state to connect/disconnect"""
+    state = data.get('s', None)
+    if state == 'on':
+        connect_sensor(sensorid, data)
+    elif state == 'off':
+        disconnect_sensor(sensorid, data)
+    else:
+        raise Exception('invalid state received: {}'.format(state))
 
-MSG_ROUTER = {
+MSG_TYPES = {
     'state': sensor_state,
     'connect': connect_sensor,
     'disconnect': disconnect_sensor,
-    'reading': sensor_reading
+    'reading': sensor_reading,
 }
 
 def on_connect(client, userdata, flags, rc):
@@ -42,16 +49,24 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     """handle receiving messages on subscribed topics"""
     _, _, sensor_id, message_type = msg.topic.split('/')
-    payload = str(msg.payload)
+    payload = msg.payload.decode("utf-8","ignore")
     print(sensor_id+' reported '+message_type+': '+payload)
 
     try:
         data = json.loads(payload)
-        fn = MSG_ROUTER.get(message_type)
+        fn = MSG_TYPES.get(message_type, None)
+        if fn is None:
+            return
         fn(sensor_id, data)
     except:
+        print('---============---')
+        print("error in on_message: ")
+        print('---============---')
         err = sys.exc_info()[0]
         print(str(err))
+        track = traceback.format_exc()
+        print(track)
+        print('---============---')
 
 
 client = mqtt.Client(client_id="mqtt_local_proxy_pi4")
